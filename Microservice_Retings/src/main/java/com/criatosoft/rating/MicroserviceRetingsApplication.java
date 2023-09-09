d6ddf47b74d1a45308a1368dd27eec9092ec9621
Lenovo <Lenovo@DESKTOP-P6F4SML>
2023-09-09 15:30:06 +0530

    ok

--- Microservice_Retings/src/main/java/com/criatosoft/rating/MicroserviceRetingsApplication.java
+++ Microservice_Retings/src/main/java/com/criatosoft/rating/MicroserviceRetingsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class MicroserviceRetingsApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceRetingsApplication.class, args);
 	}
