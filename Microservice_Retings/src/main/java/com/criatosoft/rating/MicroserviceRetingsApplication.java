1323e6aeedad318841f118b82a35cea940494377
Lenovo <Lenovo@DESKTOP-P6F4SML>
2023-09-09 15:17:42 +0530

    MS_RatingService

---
+++ Microservice_Retings/src/main/java/com/criatosoft/rating/MicroserviceRetingsApplication.java
@@ -0,0 +1,14 @@
+package com.criatosoft.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MicroserviceRetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRetingsApplication.class, args);
+	}
+
+}
