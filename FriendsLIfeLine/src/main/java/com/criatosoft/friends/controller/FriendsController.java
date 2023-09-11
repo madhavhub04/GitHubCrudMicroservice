a67d8c9a292df1defed0038781888d228c3e2ec4
madhavhub04 <ghatol.madhav@gmail.com>
2023-09-11 15:59:11 +0530

    1 mathod add

--- FriendsLIfeLine/src/main/java/com/criatosoft/friends/controller/FriendsController.java
+++ FriendsLIfeLine/src/main/java/com/criatosoft/friends/controller/FriendsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,7 @@ public class FriendsController {
 		Friends singleF = friendsServicei.getSingleF(id);
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<?> removeOne(String id){
 		friendsServicei.deleteOne(id);
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
